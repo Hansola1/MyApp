bb2a4e35ac42b0dc6079e2c37967d9401a6a6fa0
Hansola1 <reynasio27@gmail.com>
2025-05-14 15:43:42 +0700

    14/05/25

---
+++ chatOnline/backend/Chat/Chat/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Chat.Models;
+
+public interface IChatClient 
+{
+    public Task ReceiveMessage(string userName, string message);
+}
+
+namespace Chat.Hubs
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public  async Task JoinChat(UserConnection connection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+            await Clients.Group(connection.ChatRoom).ReceiveMessage("Admin", $"{connection.UserName} добавился в чат");
+        }
+    }
+}
